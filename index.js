f84e3c4a849d089ed9d79588bb3d3574192a2f4c
Angelina Montanez <angelinamontanez80@gmail.com>
2019-01-18 02:30:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 25
-var message = ` is ${height} 25 inches tall`
+var message = ` is ${height} 50 inches tall`
 
 module.exports = {name, height, message}
